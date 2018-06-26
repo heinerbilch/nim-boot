787566171e9e001d70f2873dff3af28dc28eec5e
Heiner Strauß <heiner@bilch.com>
2018-06-26 09:42:59 +0200

    Initial commit.

---
+++ src/test/java/com/bilch/nim/NimApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bilch.nim;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NimApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
