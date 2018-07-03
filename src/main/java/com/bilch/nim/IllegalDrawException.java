51273fdd252ba3d2f0daee5fdea25ed0272f4532
Heiner Strauß <heiner@bilch.com>
2018-07-03 15:45:50 +0200

    documentation and logging

--- src/main/java/com/bilch/nim/IllegalDrawException.java
+++ src/main/java/com/bilch/nim/IllegalDrawException.java
@@ -1,14 +1,29 @@
 package com.bilch.nim;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * A draw exception is thrown when the operation on the stack would break the
+ * nim rules. The exception translates to a 404 error.
+ * 
+ * @author heiner
+ *
+ */
 @ResponseStatus(org.springframework.http.HttpStatus.NOT_FOUND)
 public class IllegalDrawException extends RuntimeException {
 
+	private static final Logger logger = LoggerFactory.getLogger(IllegalDrawException.class);
 	private static final long serialVersionUID = 1L;
 
-	public IllegalDrawException(String message) {
-		super("illegal draw: " + message);
+	/**
+	 * @param message
+	 *            the detailed error on the nim stack.
+	 */
+	public IllegalDrawException(final String message) {
+		super("Illegal draw: " + message);
+		logger.error("Illegal draw: " + message);
 	}
 
 }
--- src/main/java/com/bilch/nim/Stack.java
+++ src/main/java/com/bilch/nim/Stack.java
@@ -1,17 +1,43 @@
 package com.bilch.nim;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Implements a simple nim stack.
+ * 
+ * @author heiner
+ *
+ */
 public class Stack {
 
+	private static final int START = 13;
 	private int value;
+	private static final Logger logger = LoggerFactory.getLogger(Stack.class);
 
+	/**
+	 * Constructs a single nim stack with 13 pieces.
+	 */
 	public Stack() {
-		value = 13;
+		value = START;
 	}
-	
+
+	/**
+	 * Gets the value of the nim stack.
+	 * 
+	 * @return the stack value
+	 */
 	public int getValue() {
 		return value;
 	}
-	
+
+	/**
+	 * Draws from the the nim stack.
+	 * 
+	 * @param number
+	 *            the number of pieces to draw from the stack
+	 * @return the new stack value
+	 */
 	public int draw(int number) {
 		if (number > 3) {
 			throw new IllegalDrawException(String.valueOf(number));
@@ -20,6 +46,7 @@ public class Stack {
 		if (value < 0) {
 			throw new IllegalDrawException(String.valueOf(value));
 		}
+		logger.info("Stack value: " + value);;
 		return value;
 	}
 
